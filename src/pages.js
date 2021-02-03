e0a5c1e89f17b5ca8b86d55f3d99d46a6269514b
purindaisuki <good12dave@gmail.com>
2021-02-04 02:00:33 +0800

    add enlist construction msg

--- src/pages.js
+++ src/pages.js
@@ -84,9 +84,26 @@ export function Potential() {
         </StyledTabs>
     )
 }
-
+const H3 = styled.h3`
+    margin-right: auto;
+    margin-top: 16rem;
+    margin-left: auto;
+    width: max-content;
+    color: ${props => props.theme.colors.onSurface};
+    a {
+        color: ${props => props.theme.colors.link};
+    }
+    a:hover {
+        color: ${props => props.theme.colors.linkHover};
+    }
+}
+`
 export const Enlist = () => {
     return (
-        <></>
+        <H3>
+            🚧施工中，請先移至
+            <a href='https://purindaisuki.github.io/TenkafuMaRecruitFilter/'
+             target="_blank" rel="noreferrer">現有版本</a>
+        </H3>
     )
 }
