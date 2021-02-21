17eeac292ee3826c3c77ca7fcfce045f4ff5dfd3
purindaisuki <good12dave@gmail.com>
2021-02-22 01:37:30 +0800

    Add item drops table

--- src/components/ItemShowcase.js
+++ src/components/ItemShowcase.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
+import { Badge } from "react-bootstrap"
 import SwitchableShowcase from './SwitchableShowcase';
 import MyMasonry from './MyMasonry';
 import MyAccordion from './MyAccordion';
 import CardTable from './CardTable';
+import { SortableTable, SortableTh, TableWrapper } from './FilterComponents';
 import itemDropData from '../gamedata/itemDrop.json';
+import stageDropData from '../gamedata/stageDrop.json';
 import { LanguageContext } from './LanguageProvider';
 
+const StyledCardHeader = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 const ItemImg = styled.img`
   width: 2.5rem;
   height: 2.5rem;
@@ -19,19 +27,24 @@ const StyledHeader = styled.div`
   font-size: medium;
   font-weight: normal;
 `
-const ItemCardHeader = (props) => {
+const ItemCardHeader = ({
+    className,
+    id
+}) => {
     const { itemString } = React.useContext(LanguageContext)
 
     return (
-        <>
+        <StyledCardHeader
+            className={className}
+        >
             <ItemImg
-                src={`${process.env.PUBLIC_URL}/img/item_${props.id}.png`}
+                src={`${process.env.PUBLIC_URL}/img/item_${id}.png`}
                 alt=''
             />
             <StyledHeader>
-                {itemString.name[props.name]}
+                {itemString.name[id]}
             </StyledHeader>
-        </>
+        </StyledCardHeader>
     )
 }
 
@@ -90,7 +103,6 @@ const ItemAccordion = styled(MyAccordion)`
         }
         .MuiAccordionSummary-content {
             display: flex;
-            align-items: center;
             justify-content: center;
             margin: 0;
         }
@@ -115,22 +127,25 @@ const ItemCard = (props) => {
 }
 
 const LayoutBtnContainer = styled.div`
-    position: absolute;
-    right: 0;
-    top: -4rem;
+    margin-bottom : 1rem;
+    > span:last-child button {
+        margin: 0;
+    }
 `
 const BtnWrapper = styled.span`
     > button {
         padding: .4rem .6rem;
-        margin-left: .6rem;
+        margin-right: .6rem;
         background-color: ${props => (
-        props.$active
-            ? props.theme.colors.secondary
-            : 'lightgray'
-    )};
+            props.$active
+                ? props.theme.colors.secondary
+                : 'lightgray'
+        )};
+        color: ${props => props.theme.colors.onSecondary};
     }
     > button:hover {
         background-color: ${props => props.theme.colors.secondary};
+        color: ${props => props.theme.colors.onSecondary};
     }
 `
 const LayoutSwitcher = (props) => {
@@ -178,11 +193,10 @@ const ItemMasonry = () => {
                     header={
                         <ItemCardHeader
                             id={item.id}
-                            name={item.name}
                         />
                     }
                     body={
-                        <CardTable>
+                        <CardTable striped={true}>
                             <CardBodyContnet
                                 drop={item.drop}
                             />
@@ -194,6 +208,164 @@ const ItemMasonry = () => {
     )
 }
 
+const StyledTh = styled(SortableTh)`
+    background-color:  ${props => props.theme.colors.secondary};
+    color:  ${props => props.theme.colors.onSecondary};
+    white-space: nowrap;
+    ${props => props.$sortable ? true : 'cursor: default;'}
+`
+const ItemWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex-wrap: nowrap;
+    margin-right: .8rem;
+    div {
+        font-size: 1rem;
+    }
+    img {
+        width: 2rem;
+        height: 2rem;
+    }
+`
+const ItemsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex-wrap: wrap;
+    > div:last-child {
+        margin: 0;
+    }
+`
+const StyledBadge = styled(Badge)`
+    background-color: lightgray;
+    color: black;
+    margin-left: .4rem;
+`
+const TableContent = (props) => {
+    const { pageString, itemString } = React.useContext(LanguageContext)
+
+    const TableHeader = () => (
+        <thead>
+            <tr>
+                {Object.entries(pageString.potential.overview.tableHead)
+                    .map((entry, idx) => {
+                        const sortable = entry[0] === 'stage' || entry[0] === 'energy'
+                        let requestSort
+                        let getSortDirection
+                        if (sortable) {
+                            requestSort = () => props.requestSort(entry[0])
+                            getSortDirection = props.getSortDirection(entry[0])
+                        }
+
+                        return (
+                            <StyledTh
+                                onClick={requestSort}
+                                direction={getSortDirection}
+                                key={idx}
+                                $sortable={sortable}
+                            >
+                                {entry[1]}
+                            </StyledTh>
+                        )
+                    })}
+            </tr>
+        </thead>
+    )
+
+    const TableBody = () => (
+        <tbody>
+            {props.sortedResult.map((stage, idx) => {
+                const itemTd = (items) => (
+                    <td>
+                        <ItemsContainer>
+                            {items.length === 0
+                                ? undefined
+                                : items.map((item, i) => (
+                                    <ItemWrapper key={i}>
+                                        <ItemCardHeader id={item.id} />
+                                        <StyledBadge pill>
+                                            {itemString.rarity[item.rarity]}
+                                        </StyledBadge>
+                                    </ItemWrapper>
+                                ))}
+                        </ItemsContainer>
+                    </td>
+                )
+
+                return (
+                    <tr key={idx}>
+                        <td>
+                            {`${stage.chapter}-${stage.stage}`}
+                        </td>
+                        {itemTd(stage.materials)}
+                        {itemTd(stage.trainItems)}
+                        {itemTd(stage.expPotions)}
+                        <td>{stage.energy}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
+    )
+
+    return (
+        <>
+            <TableHeader />
+            <TableBody />
+        </>
+    )
+}
+
+const ItemTableWrapper = styled(TableWrapper)`
+    overflow-x: auto;
+    height: calc(100vh - 14.5rem);
+`
+const ItemTable = () => {
+    const sortFunc = (sortableItems, sortConfig) => {
+        const toStageKey = key => {
+            return (
+                parseInt(key.chapter) * 1000 +
+                parseInt(key.stage.split(' ')[0]) * 10 +
+                (key.stage.includes('free') ? 1 : 0) +
+                (key.stage.includes('-') ? parseInt(key.stage.split('-')[1]) : 0)
+            )
+        }
+
+        sortableItems.sort((a, b) => {
+            let aKey
+            let bKey
+            if (sortConfig.key === 'stage') {
+                aKey = toStageKey(a)
+                bKey = toStageKey(b)
+            } else {
+                aKey = a[sortConfig.key]
+                bKey = b[sortConfig.key]
+            }
+            if (aKey < bKey) {
+                return sortConfig.direction === 'asc' ? -1 : 1
+            }
+            if (aKey > bKey) {
+                return sortConfig.direction === 'asc' ? 1 : -1
+            }
+            return 0
+        })
+    }
+
+    return (
+        <ItemTableWrapper>
+            <SortableTable
+                sortFunc={sortFunc}
+                defaultSortKey={'stage'}
+                result={stageDropData}
+                striped={false}
+                border={true}
+            >
+                <TableContent />
+            </SortableTable>
+        </ItemTableWrapper>
+    )
+}
+
 export default function ItemShowcase() {
     return (
         <SwitchableShowcase
@@ -201,7 +373,7 @@ export default function ItemShowcase() {
             layoutSwitcher={<LayoutSwitcher />}
             items={[
                 { layout: 'Masonry', content: <ItemMasonry /> },
-                { layout: 'Table', content: <div /> },
+                { layout: 'Table', content: <ItemTable /> },
             ]}
         />
     )
