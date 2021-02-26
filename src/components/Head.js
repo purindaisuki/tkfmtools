c04c2dcf3cc85c638d3359c6a8beb7972185b909
purindaisuki <good12dave@gmail.com>
2021-02-27 04:10:55 +0800

    add prefix to icon and manifest path

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,26 +3,26 @@ import Helmet from "react-helmet"
 
 export default () => (
     <Helmet>
-        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="a/pple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/apple-touch-icon-60x60.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-        <link rel="icon" type="image/png" href="/favicon-196x196.png" sizes="196x196" />
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-        <link rel="icon" type="image/png" href="/favicon-128.png" sizes="128x128" />
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/tkfmtools/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/tkfmtools/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/tkfmtools/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/tkfmtools/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/tkfmtools/apple-touch-icon-60x60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/tkfmtools/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/tkfmtools/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/tkfmtools/apple-touch-icon-152x152.png" />
+        <link rel="icon" type="image/png" href="/tkfmtools/favicon-196x196.png" sizes="196x196" />
+        <link rel="icon" type="image/png" href="/tkfmtools/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" href="/tkfmtools/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/tkfmtools/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/tkfmtools/favicon-128.png" sizes="128x128" />
         <meta name="application-name" content="&nbsp;"/>
         <meta name="msapplication-TileColor" content="#FFFFFF" />
-        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
-        <meta name="msapplication-square70x70logo" content="/mstile-70x70.png" />
-        <meta name="msapplication-square150x150logo" content="/mstile-150x150.png" />
-        <meta name="msapplication-square310x310logo" content="/mstile-310x310.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileImage" content="/tkfmtools/mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="/tkfmtools/mstile-70x70.png" />
+        <meta name="msapplication-square150x150logo" content="/tkfmtools/mstile-150x150.png" />
+        <meta name="msapplication-square310x310logo" content="/tkfmtools/mstile-310x310.png" />
+        <link rel="manifest" href="/tkfmtools/manifest.json" />
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
         <meta name="title" content="天下布魔工具箱" />
         <meta name="description" content="提供天下布魔中資料查詢、處理，減輕娜娜負擔。" />
