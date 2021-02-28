52369b5de6ba8bf2ac446d544c62e72540f1c831
purindaisuki <good12dave@gmail.com>
2021-02-28 23:37:24 +0800

    add gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+	siteMetadata: {
+		title: `Tenkafu MA! toolbox`,
+	},
+	pathPrefix: `/tkfmtools`,
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-styled-components`,
+		`gatsby-plugin-material-ui`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/src/images`,
+				name: `images`,
+			},
+		},
+		{
+			resolve: "gatsby-plugin-google-tagmanager",
+			options: {
+				id: "GTM-KHV3HZD",
+				includeInDevelopment: true,
+				defaultDataLayer: {
+					platform: "gatsby",
+				},
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				trackingIds: [
+					"G-Z22RL2KGP1",
+				],
+			},
+			gtagConfig: {
+				anonymize_ip: true,
+				cookie_expires: 0,
+			},
+			pluginConfig: {
+				head: false,
+				respectDNT: true,
+			},
+		},
+	],
+}
