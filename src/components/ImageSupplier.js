ad73e5e01fc54ab2d6ad4795ce7ad508fc14726c
purindaisuki <good12dave@gmail.com>
2021-03-11 22:20:11 +0800

    rm redundant import

--- src/components/ImageSupplier.js
+++ src/components/ImageSupplier.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
-import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage } from "gatsby-plugin-image";
 import BackgroundImage from 'gatsby-background-image'
 
 // Helper functions.
