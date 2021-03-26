acabee6d601deed5583db1cbfce9db9f62c49e07
purindaisuki <good12dave@gmail.com>
2021-03-26 13:07:26 +0800

    update texts

---
+++ scripts/retrieve-text.js
@@ -0,0 +1,35 @@
+const page_tr = require('../src/data/string/page_zh-TW.json');
+const char_tr = require('../src/data/string/character_zh-TW.json');
+const item_tr = require('../src/data/string/item_zh-TW.json');
+const page_en = require('../src/data/string/page_en.json');
+const char_en = require('../src/data/string/character_en.json');
+const item_en = require('../src/data/string/item_en.json');
+const fs = require('fs');
+
+const strings = [[page_tr, page_en], [char_tr, char_en], [item_tr, item_en]]
+
+const allTexts = {}
+
+const iterate = (obj, parentKey, newData, lang) => {
+    Object.keys(obj).forEach(key => {
+        if (typeof obj[key] === 'object') {
+            iterate(obj[key], parentKey + '_' + key, newData, lang)
+        } else {
+            if (newData[parentKey + '_' + key]) {
+                newData[parentKey + '_' + key][lang] = obj[key]
+            }
+            else {
+                newData[parentKey + '_' + key] = { [lang]: obj[key] }
+            }
+        }
+    })
+}
+
+strings.forEach((string, i) => {
+    const data = {}
+    iterate(string[0], 'root', data, 'zh-TW')
+    iterate(string[1], 'root', data, 'en')
+    fs.writeFile('./texts_' + i + '.json', JSON.stringify(data), (err) => {
+        if (err) console.log(err);
+    })
+})
