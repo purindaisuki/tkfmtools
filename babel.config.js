39849daf6b3d2fdba486bea410720e2fd7ccb0cc
purindaisuki <good12dave@gmail.com>
2021-08-02 21:18:32 +0800

    config gatsby testing

---
+++ babel.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  plugins: [
+    [
+      "transform-imports",
+      {
+        "@material-ui/core": {
+          transform: "@material-ui/core/esm/${member}",
+          preventFullImport: true,
+        },
+        "@material-ui/lab": {
+          transform: "@material-ui/lab/esm/${member}",
+          preventFullImport: true,
+        },
+      },
+    ],
+    "@babel/plugin-proposal-class-properties",
+    "@babel/proposal-object-rest-spread",
+    ["@babel/plugin-proposal-private-methods", { loose: false }],
+  ],
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+    ["babel-preset-gatsby", { targets: { esmodules: true } }],
+  ],
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-modules-commonjs"],
+    },
+  },
+};
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)?$": "ts-jest",
+  },
   moduleDirectories: ["node_modules", "src"],
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/file-mock.js",
+    "^@reach/router(.*)": "<rootDir>/node_modules/@gatsbyjs/reach-router$1",
   },
   testPathIgnorePatterns: ["node_modules", "\\.cache", "<rootDir>.*/public"],
-  transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
+  transformIgnorePatterns: ["node_modules/(?!(gatsby|@material-ui)/)"],
   globals: {
     __PATH_PREFIX__: "",
   },
