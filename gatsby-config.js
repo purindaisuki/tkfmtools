3c57af0bb531de12f9e0657b2ebb20d8edcb8d49
purindaisuki <good12dave@gmail.com>
2021-08-02 22:52:21 +0800

    remove unused packages; update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-postcss`,
     `gatsby-plugin-material-ui`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
