1531ec7fe0757e4fe19608d23453edc3e43bc04b
purindaisuki <good12dave@gmail.com>
2021-10-09 21:51:04 +0800

    refactor InfiniteLoader in TS

--- src/components/InfiniteLoader.tsx
+++ src/components/InfiniteLoader.tsx
@@ -2,60 +2,84 @@ import React, { useEffect, useReducer, useRef } from "react";
 import styled from "styled-components";
 import { CircularProgress, List } from "@material-ui/core";
 
+type InfiniteLoaderState<T> = {
+  items: T[];
+  isFetching: boolean;
+  shouldLoad: boolean;
+};
+
+type InfiniteLoaderAction<T> =
+  | { type: "PUSH" | "UNSHIFT"; items: T[] }
+  | { type: "FLAG_FECTHING"; isFetching: boolean }
+  | { type: "FLAG_SHOULD_LOAD"; shouldLoad: boolean }
+  | { type: "RESET" };
+
 const initState = {
   items: [],
   isFetching: false,
-  loadMore: true,
-};
+  shouldLoad: true,
+} as InfiniteLoaderState<any>;
 
-const reducer = (state, action) => {
+const reducer = <T,>(
+  state: InfiniteLoaderState<T>,
+  action: InfiniteLoaderAction<T>
+) => {
   switch (action.type) {
     case "PUSH":
       return { ...state, items: state.items.concat(action.items) };
     case "UNSHIFT":
       return { ...state, items: action.items.concat(state.items) };
+    case "FLAG_FECTHING":
+      return { ...state, isFetching: action.isFetching };
+    case "FLAG_SHOULD_LOAD":
+      return { ...state, shouldLoad: action.shouldLoad };
     case "RESET":
       return initState;
-    case "FETCH":
-      return { ...state, isFetching: action.isFetching };
-    case "LOAD_MORE":
-      return { ...state, loadMore: action.loadMore };
     default:
-      throw new Error();
+      throw new Error("Unexpected action type");
   }
 };
 
-const InfiniteLoader = ({
+type InfiniteLoaderProps<T> = {
+  listenToUpdate: (callback: (newItems: T[]) => void) => void | (() => void);
+  fetchItem: () => Promise<T[]>;
+  renderItem: (i: T) => JSX.Element;
+  shouldReset: boolean;
+  onReset: () => void;
+};
+
+const InfiniteLoader = <T,>({
   listenToUpdate,
   fetchItem,
   renderItem,
   shouldReset,
   onReset,
-}) => {
+}: InfiniteLoaderProps<T>) => {
   const [state, dispatch] = useReducer(reducer, initState);
 
-  const bottomBoundaryRef = useRef();
+  const bottomBoundaryRef = useRef<HTMLDivElement>(null!);
 
-  useEffect(() => {
-    if (!bottomBoundaryRef.current) {
-      return;
+  // subscribe to scroller and trigger loading when bottom div intersects viewport
+  useEffect((): void | (() => void) => {
+    if (!bottomBoundaryRef?.current) {
+      throw Error("bottomBoundaryRef is not assigned");
     }
 
     const scrollObserver = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.intersectionRatio > 0) {
-          dispatch({ type: "LOAD_MORE", loadMore: true });
+          dispatch({ type: "FLAG_SHOULD_LOAD", shouldLoad: true });
         }
       });
-    }).observe(bottomBoundaryRef.current);
+    });
 
-    if (scrollObserver) {
-      return () => scrollObserver.unobeserve(bottomBoundaryRef.current);
-    }
+    scrollObserver.observe(bottomBoundaryRef.current);
+
+    return () => scrollObserver.unobserve(bottomBoundaryRef.current);
   }, [bottomBoundaryRef]);
 
   useEffect(() => {
-    if (!state.loadMore) {
+    if (!state.shouldLoad) {
       return;
     }
 
@@ -63,12 +87,12 @@ const InfiniteLoader = ({
     let didCancel = false;
 
     const fetch = async () => {
-      dispatch({ type: "FETCH", isFetching: true });
+      dispatch({ type: "FLAG_FECTHING", isFetching: true });
       try {
         const items = await fetchItem();
 
         if (!items) {
-          // fetchItem is not ready
+          // fetchItem fails
           return;
         }
 
@@ -76,14 +100,14 @@ const InfiniteLoader = ({
           if (items.length !== 0) {
             dispatch({ type: "PUSH", items: items });
           }
-          dispatch({ type: "FETCH", isFetching: false });
-          dispatch({ type: "LOAD_MORE", loadMore: false });
+          dispatch({ type: "FLAG_FECTHING", isFetching: false });
+          dispatch({ type: "FLAG_SHOULD_LOAD", shouldLoad: false });
         }
       } catch (error) {
         if (!didCancel) {
           console.log(error);
-          dispatch({ type: "FETCH", isFetching: false });
-          dispatch({ type: "LOAD_MORE", loadMore: false });
+          dispatch({ type: "FLAG_FECTHING", isFetching: false });
+          dispatch({ type: "FLAG_SHOULD_LOAD", shouldLoad: false });
         }
       }
     };
@@ -93,9 +117,9 @@ const InfiniteLoader = ({
     return () => {
       didCancel = true;
     };
-  }, [fetchItem, state.loadMore]);
+  }, [fetchItem, state.shouldLoad]);
 
-  useEffect(() => {
+  useEffect((): void | (() => void) => {
     const cleanup = listenToUpdate((newItems) => {
       dispatch({ type: "UNSHIFT", items: newItems });
     });
