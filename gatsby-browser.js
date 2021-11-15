aeb526cf75aca0c6ec3d88881557684b8135c757
purindaisuki <good12dave@gmail.com>
2021-11-15 19:06:04 +0800

    use language and path prefix from configs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,22 @@
+import langConfig from "./src/languageConfig.json";
+
 export { wrapRootElement, wrapPageElement } from "./gatsby-ssr";
 
 export const onRouteUpdate = ({ location }) => {
+  if (!__PATH_PREFIX__) return;
+
   const locale = location.pathname.split("/")[2];
 
-  if (["en", "ja", "ko"].includes(locale)) {
+  if (
+    Object.keys(langConfig)
+      .filter((key) => !langConfig[key].default)
+      .includes(locale)
+  ) {
+    // localize manifest filename
     const manifestEl = document.head.querySelector('link[rel="manifest"]');
     manifestEl.setAttribute(
       "href",
-      `/tkfmtools/manifest_${locale}.webmanifest`
+      `${__PATH_PREFIX__}/manifest_${locale}.webmanifest`
     );
   }
 };
