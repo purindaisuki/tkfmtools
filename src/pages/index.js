982d502e2e468d79f330f8c988410c951bbcd923
purindaisuki <good12dave@gmail.com>
2022-06-10 01:17:56 +0800

    fix layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,81 +5,81 @@ import { useLanguage } from "containers/LanguageProvider";
 import IconButton from "components/IconButton";
 import Accordion from "components/Accordion";
 import {
-  LogMsg,
-  SiteDescriptions,
-  SiteSetting,
-  SiteUpdateLog,
-  SiteLicense,
+	LogMsg,
+	SiteDescriptions,
+	SiteSetting,
+	SiteUpdateLog,
+	SiteLicense,
 } from "components/SiteAccordionBody";
 import Modal from "components/Modal";
 import { ExpandMoreIcon, NoteIcon } from "components/icon";
 
 const UpdateModal = ({ open, onClose }) => {
-  const { pageString } = useLanguage();
-  const latestMsg = pageString.index.updateLog.content[0];
+	const { pageString } = useLanguage();
+	const latestMsg = pageString.index.updateLog.content[0];
 
-  return (
-    <MsgModal
-      title={`${pageString.index.helmet.title} ${latestMsg.version}`}
-      open={open}
-      onClose={onClose}
-      ariaLabelledby="update-modal-title"
-    >
-      <ModalBody>
-        {latestMsg.content.map((msg, ind) => (
-          <LogMsg msg={msg} alwaysOpen={true} key={ind} />
-        ))}
-      </ModalBody>
-    </MsgModal>
-  );
+	return (
+		<MsgModal
+			title={`${pageString.index.helmet.title} ${latestMsg.version}`}
+			open={open}
+			onClose={onClose}
+			ariaLabelledby="update-modal-title"
+		>
+			<ModalBody>
+				{latestMsg.content.map((msg, ind) => (
+					<LogMsg msg={msg} alwaysOpen={true} key={ind} />
+				))}
+			</ModalBody>
+		</MsgModal>
+	);
 };
 
 const MsgModal = styled(Modal)`
-  > div:nth-child(3) {
-    width: 40%;
-    @media screen and (max-width: 992px) {
-      width: 80%;
-    }
-    @media screen and (max-width: 768px) {
-      width: 90%;
-    }
-    top: 20%;
-  }
+	> div:nth-child(3) {
+		width: 40%;
+		@media screen and (max-width: 992px) {
+			width: 80%;
+		}
+		@media screen and (max-width: 768px) {
+			width: 90%;
+		}
+		top: 20%;
+	}
 `;
 const ModalBody = styled.div`
-  margin: 0;
-  &&& .MuiAccordion-root,
-  &&& .MuiAccordionSummary-root {
-    border: none;
-    cursor: text;
-  }
+	margin: 0;
+	&&& .MuiAccordion-root,
+	&&& .MuiAccordionSummary-root {
+		border: none;
+		cursor: text;
+	}
 `;
 
 const UpdateNote = ({ setVersion, latestVersion, $unread }) => {
-  const { pageString } = useLanguage();
+	const { pageString } = useLanguage();
 
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const handleModal = (boolean) => () => {
-    setOpen(boolean);
+	const handleModal = (boolean) => () => {
+		setOpen(boolean);
 
-    if (boolean && $unread) {
-      setVersion(latestVersion);
-    }
-  };
+		if (boolean && $unread) {
+			setVersion(latestVersion);
+		}
+	};
 
-  return (
-    <>
-      <NoteButton
-        onClick={handleModal(true)}
-        tooltipText={pageString.index.noteButtonTooltip}
-        $unread={$unread}
-      >
-        {NoteIcon}
-      </NoteButton>
-      <UpdateModal open={open} onClose={handleModal(false)} />
-    </>
-  );
+	return (
+		<>
+			<NoteButton
+				onClick={handleModal(true)}
+				tooltipText={pageString.index.noteButtonTooltip}
+				$unread={$unread}
+			>
+				{NoteIcon}
+			</NoteButton>
+			<UpdateModal open={open} onClose={handleModal(false)} />
+		</>
+	);
 };
 
 const unreadAnimation = keyframes`
@@ -100,148 +100,148 @@ const unreadAnimation = keyframes`
     }
 `;
 const NoteButton = styled(IconButton)`
-  position: relative;
-  padding: 0;
-  svg {
-    width: 1.2rem;
-    height: 1.2rem;
-    fill: ${({ theme }) => theme.colors.onSurface};
-  }
-  &:after {
-    position: absolute;
-    content: "";
-    right: -0.3rem;
-    top: -0.3rem;
-    background-color: red;
-    border-radius: 100%;
-    animation: ${unreadAnimation} 1.5s ease-in-out infinite;
-    ${({ $unread }) => ($unread ? "width: .6rem; height: .6rem;" : "")}
-  }
+	position: relative;
+	padding: 0;
+	svg {
+		width: 1.2rem;
+		height: 1.2rem;
+		fill: ${({ theme }) => theme.colors.onSurface};
+	}
+	&:after {
+		position: absolute;
+		content: "";
+		right: -0.3rem;
+		top: -0.3rem;
+		background-color: red;
+		border-radius: 100%;
+		animation: ${unreadAnimation} 1.5s ease-in-out infinite;
+		${({ $unread }) => ($unread ? "width: .6rem; height: .6rem;" : "")}
+	}
 `;
 
 const Home = () => {
-  const { pageString } = useLanguage();
-  const latestMsg = pageString.index.updateLog.content[0];
+	const { pageString } = useLanguage();
+	const latestMsg = pageString.index.updateLog.content[0];
 
-  const [expanded, setExpanded] = useState(0);
-  const [localVersion, setVersion] = useLocalStorage("last-read-version");
+	const [expanded, setExpanded] = useState(0);
+	const [localVersion, setVersion] = useLocalStorage("last-read-version");
 
-  const [deferredPrompt, setDeferredPrompt] = useState(null);
+	const [deferredPrompt, setDeferredPrompt] = useState(null);
 
-  const handleExpand = (panel) => (_, isExpanded) => {
-    setExpanded(isExpanded ? panel : null);
-  };
+	const handleExpand = (panel) => (_, isExpanded) => {
+		setExpanded(isExpanded ? panel : null);
+	};
 
-  const clearDeferredPrompt = () => setDeferredPrompt(null);
+	const clearDeferredPrompt = () => setDeferredPrompt(null);
 
-  useEffect(() => {
-    const promptHandler = (e) => {
-      e.preventDefault();
-      setDeferredPrompt(e);
-    };
+	useEffect(() => {
+		const promptHandler = (e) => {
+			e.preventDefault();
+			setDeferredPrompt(e);
+		};
 
-    window.addEventListener("beforeinstallprompt", promptHandler);
+		window.addEventListener("beforeinstallprompt", promptHandler);
 
-    return () =>
-      window.removeEventListener("beforeinstallprompt", promptHandler);
-  }, []);
+		return () =>
+			window.removeEventListener("beforeinstallprompt", promptHandler);
+	}, []);
 
-  return (
-    <HomeContainer>
-      <Header>
-        <span>{pageString.index.helmet.title}</span>
-        <span>{latestMsg.version}</span>
-        <UpdateNote
-          setVersion={setVersion}
-          latestVersion={latestMsg.version}
-          $unread={localVersion !== latestMsg.version}
-        />
-      </Header>
-      {[
-        {
-          header: pageString.index.about.header,
-          body: <SiteDescriptions />,
-        },
-        {
-          header: pageString.index.setting.header,
-          body: (
-            <SiteSetting
-              deferredPrompt={deferredPrompt}
-              clearDeferredPrompt={clearDeferredPrompt}
-            />
-          ),
-        },
-        {
-          header: pageString.index.updateLog.header,
-          body: <SiteUpdateLog />,
-        },
-        {
-          header: pageString.index.license.header,
-          body: <SiteLicense />,
-        },
-      ].map((item, ind) => (
-        <DescriptionAccordion
-          expanded={expanded === ind}
-          onChange={handleExpand(ind)}
-          expandIcon={ExpandMoreIcon}
-          title={item.header}
-          content={item.body}
-          key={ind}
-        />
-      ))}
-    </HomeContainer>
-  );
+	return (
+		<HomeContainer>
+			<Header>
+				<span>{pageString.index.helmet.title}</span>
+				<span>{latestMsg.version}</span>
+				<UpdateNote
+					setVersion={setVersion}
+					latestVersion={latestMsg.version}
+					$unread={localVersion !== latestMsg.version}
+				/>
+			</Header>
+			{[
+				{
+					header: pageString.index.about.header,
+					body: <SiteDescriptions />,
+				},
+				{
+					header: pageString.index.setting.header,
+					body: (
+						<SiteSetting
+							deferredPrompt={deferredPrompt}
+							clearDeferredPrompt={clearDeferredPrompt}
+						/>
+					),
+				},
+				{
+					header: pageString.index.updateLog.header,
+					body: <SiteUpdateLog />,
+				},
+				{
+					header: pageString.index.license.header,
+					body: <SiteLicense />,
+				},
+			].map((item, ind) => (
+				<DescriptionAccordion
+					expanded={expanded === ind}
+					onChange={handleExpand(ind)}
+					expandIcon={ExpandMoreIcon}
+					title={item.header}
+					content={item.body}
+					key={ind}
+				/>
+			))}
+		</HomeContainer>
+	);
 };
 
 const HomeContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  &&& > div {
-    margin-bottom: 2rem;
-  }
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	&&& > div {
+		margin-bottom: 2rem;
+	}
 `;
 const Header = styled.div`
-  width: 90%;
-  max-width: 1000px;
-  @media screen and (max-width: 600px) {
-    width: 100%;
-  }
-  margin-top: 1rem;
-  margin-bottom: 2rem;
-  font-size: x-large;
-  font-weight: bold;
-  color: ${({ theme }) => theme.colors.onSurface};
-  > span:nth-child(2) {
-    margin: 0.4rem;
-    font-size: large;
-  }
+	width: 90%;
+	max-width: 1000px;
+	@media screen and (max-width: 600px) {
+		width: 100%;
+	}
+	margin-top: 1rem;
+	margin-bottom: 2rem;
+	font-size: x-large;
+	font-weight: bold;
+	color: ${({ theme }) => theme.colors.onSurface};
+	> span:nth-child(2) {
+		margin: 0.4rem;
+		font-size: large;
+	}
 `;
 const DescriptionAccordion = styled(Accordion)`
-  width: 90%;
-  max-width: 1000px;
-  @media screen and (max-width: 600px) {
-    width: 100%;
-  }
-  border: 1px solid ${({ theme }) => theme.colors.border};
-  border-radius: 0.25rem;
-  & {
-    box-shadow: 0 0 0.15em lightgray;
-  }
-  > .MuiAccordionSummary-root {
-    font-size: large;
-    padding: 0.75rem 1.25rem;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-    border-bottom: 0px solid ${({ theme }) => theme.colors.border};
-  }
-  > .MuiAccordionSummary-root.Mui-expanded {
-    border-bottom: 1px solid ${({ theme }) => theme.colors.border};
-  }
-  .MuiAccordionDetails-root {
-    margin: 1rem;
-    padding: 0;
-  }
+	width: 90%;
+	max-width: 1000px;
+	@media screen and (max-width: 600px) {
+		width: 100%;
+	}
+	border: 1px solid ${({ theme }) => theme.colors.border};
+	border-radius: 0.25rem;
+	& {
+		box-shadow: 0 0 0.15em lightgray;
+	}
+	> .MuiAccordionSummary-root {
+		font-size: large;
+		padding: 0.75rem 1.25rem;
+		border-bottom-right-radius: 0;
+		border-bottom-left-radius: 0;
+		border-bottom: 0px solid ${({ theme }) => theme.colors.border};
+	}
+	> .MuiAccordionSummary-root.Mui-expanded {
+		border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+	}
+	.MuiAccordionDetails-root {
+		margin: 1rem;
+		padding: 0;
+	}
 `;
 
 export default Home;
